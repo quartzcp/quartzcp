a8b0a3085b8b0aded89bca1e835b7aab39fa7c5a
Cameron Clough <cameronjclough@gmail.com>
2020-08-16 20:29:55 +0100

    chore: add Vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
     <h1>Quartz CP</h1>
   </div>
 </template>
