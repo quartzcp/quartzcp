c436e59988a2739a6f49f59aaf543da8f95254c3
Cameron Clough <cameronjclough@gmail.com>
2020-08-20 17:57:52 +0100

    refactor(vuex): modularise store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,10 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import rootState from './state';
 
 Vue.use(Vuex);
 
+/** @type import('vuex').Store<typeof rootState> */
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: rootState,
 });
